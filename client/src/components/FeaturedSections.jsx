4566dc5995a84b9e386ddc5e16aa5c4326138f55
toffly <toffly@outlook.com>
2026-08-07 14:03:48 +0700

    feat: add MovieCard component and integrate with FeaturedSections

--- client/src/components/FeaturedSections.jsx
+++ client/src/components/FeaturedSections.jsx
@@ -1,6 +1,8 @@
 import { ArrowRight } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import BlurCircle from "./BlurCircle";
+import { dummyShowsData } from "../assets/assets";
+import MovieCard from "./MovieCard";
 
 const FeaturedSections = () => {
   const navigate = useNavigate();
@@ -19,7 +21,11 @@ const FeaturedSections = () => {
         </button>
       </div>
 
-      <div></div>
+      <div className="flex flex-wrap max-sm:justify-center gap-8 mt-8">
+        {dummyShowsData.slice(0, 4).map((show) => (
+          <MovieCard key={show._id} movie={show} />
+        ))}
+      </div>
 
       <div className="flex justify-center mt-20">
         <button
