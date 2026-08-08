d2d11b639cb015ce30aff48252f9bb88d004c94b
toffly <toffly@outlook.com>
2026-08-08 13:33:31 +0700

    feat: implement Movies page to display available movies with dynamic content

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,9 +1,24 @@
-
+import { dummyShowsData } from "../assets/assets";
+import BlurCircle from "../components/BlurCircle";
+import MovieCard from "../components/MovieCard";
 
 const Movies = () => {
-  return (
-    <div>Movies</div>
-  )
-}
+  return dummyShowsData.length > 0 ? (
+    <div className="relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 pb-20 overflow-hidden min-h-[8ovh]">
+      <BlurCircle top="150px" left="0px"/>
+      <BlurCircle bottom="50px" right="50px"/>
+      <h1 className="text-lg font-medium my-4">Now Showing</h1>
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+        {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie._id}/>
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold text-center">No movies avaliable</h1>
+    </div>
+  );
+};
 
-export default Movies
+export default Movies;
