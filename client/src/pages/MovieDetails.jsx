601927bcecc6bb3f7aa5fcbfc0440ebbd34e0201
toffly <toffly@outlook.com>
2026-08-08 14:59:45 +0700

    feat: enhance MovieDetails page with related movie suggestions and navigation

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,12 +1,14 @@
 import { useMemo } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
 import BlurCircle from "../components/BlurCircle";
 import { Heart, PlayCircleIcon, StarIcon } from "lucide-react";
 import timeFormat from "../lib/timeFormat";
 import DateSelect from "../components/DateSelect";
+import MovieCard from "../components/MovieCard";
 
 const MovieDetails = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const show = useMemo(() => {
     const movie = dummyShowsData.find((show) => show._id === id);
@@ -76,7 +78,26 @@ const MovieDetails = () => {
         </div>
       </div>
 
-      <DateSelect dateTime={show.dateTime} id={id}/>
+      <DateSelect dateTime={show.dateTime} id={id} />
+
+      <p className="text-lg font-medium mt-20 mb-8">You May Also Like</p>
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+        {dummyShowsData.slice(0, 4).map((movie, index) => (
+          <MovieCard key={index} movie={movie} />
+        ))}
+      </div>
+
+      <div
+        onClick={() => {
+          navigate("/movies");
+          scrollTo(0,0)
+        }}
+        className="flex justify-center mt-20"
+      >
+        <button className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer">
+          Show more
+        </button>
+      </div>
     </div>
   ) : (
     <div>Loading...</div>
