975c49ecebc3199436c47432a2676f8077e4aa36
toffly <toffly@outlook.com>
2026-08-08 13:35:11 +0700

    feat: implement Favorite page to display user's favorite movies with dynamic content

--- client/src/pages/Favorite.jsx
+++ client/src/pages/Favorite.jsx
@@ -1,9 +1,24 @@
-
+import { dummyShowsData } from "../assets/assets";
+import BlurCircle from "../components/BlurCircle";
+import MovieCard from "../components/MovieCard";
 
 const Favorite = () => {
-  return (
-    <div>Favorite</div>
-  )
-}
+  return dummyShowsData.length > 0 ? (
+    <div className="relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 pb-20 overflow-hidden min-h-[8ovh]">
+      <BlurCircle top="150px" left="0px"/>
+      <BlurCircle bottom="50px" right="50px"/>
+      <h1 className="text-lg font-medium my-4">Your Favorite Movies</h1>
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+        {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie._id}/>
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold text-center">No Favorite avaliable</h1>
+    </div>
+  );
+};
 
-export default Favorite
+export default Favorite;
