7566d6ea197659c1c6fe691a534d1ff1751a8068
toffly <toffly@outlook.com>
2026-08-08 13:10:25 +0700

    feat: add TrailersSection component and integrate into Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import FeaturedSections from "../components/FeaturedSections"
 import HeroSection from "../components/HeroSection"
+import TrailersSection from "../components/TrailersSection"
 
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <HeroSection/>
       <FeaturedSections/>
+      <TrailersSection/>
     </div>
   )
 }
