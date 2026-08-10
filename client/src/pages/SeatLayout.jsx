f6a7f1993b2e52b182aec47309e2d6c60dc84351
toffly <toffly@outlook.com>
2026-08-10 11:43:07 +0700

    feat: implement SeatLayout component with show selection and loading state

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,9 +1,64 @@
-
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
+import Loading from "../components/Loading";
+import { ClockIcon } from "lucide-react";
+import isoTimeFormat from "../lib/isoTimeFormat";
+import BlurCircle from "../components/BlurCircle";
 
 const SeatLayout = () => {
-  return (
-    <div>SeatLayout</div>
-  )
-}
+  const { id, date } = useParams();
+  const [selectedSeats, setSelectedSeats] = useState([]);
+  const [selectedTime, setSelectedTime] = useState([]);
+  const [show, setShow] = useState(null);
 
-export default SeatLayout
+  const navigate = useNavigate();
+
+  const getShow = async () => {
+    const foundShow = dummyShowsData.find((show) => show._id === id);
+    if (foundShow) {
+      setShow({
+        movie: foundShow,
+        dateTime: dummyDateTimeData,
+      });
+    }
+  };
+
+  useEffect(() => {
+    const loadShow = async () => {
+      await getShow();
+    };
+
+    loadShow();
+  }, [id]);
+
+  return show ? (
+    <div className="flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50">
+      <div className="w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 h-max md:sticky md:top-30">
+        <p className="text-lg font-semibold px-6">Avaliable Timimgs</p>
+        <div className="mt-5 space-y-1">
+          {show.dateTime[date].map((item) => (
+            <div
+              key={item.time}
+              onClick={() => setSelectedTime(item)}
+              className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${selectedTime?.time === item.time ? "bg-primary text-white" : "hover:bg-primary/20"} `}
+            >
+              <ClockIcon className="w-4 h-4" />
+              <p className="text-sm">{isoTimeFormat(item.time)}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative flex-1 flex flex-col items-center max-md:mt-16">
+        <BlurCircle top="-100px" left="-100px"/>
+        <BlurCircle top="0" left="0"/>
+        <h1 className="text-2xl font-semibold">Select your seat</h1>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
+
+export default SeatLayout;
