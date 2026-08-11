4ec344e6a2a9d570e003a870a938bcdb0bd2a64a
toffly <toffly@outlook.com>
2026-08-11 14:44:40 +0700

    feat: add BlurCircle component to Dashboard for enhanced visual layout

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react";
 import { dummyDashboardData } from "../../assets/assets";
 import Loading from "../../components/Loading";
 import Title from "../../components/admin/Title";
+import BlurCircle from "../../components/BlurCircle";
 
 const Dashboard = () => {
   const currency = import.meta.env.VITE_CURRENCY;
@@ -58,6 +59,21 @@ const Dashboard = () => {
   return !loading ? (
     <>
       <Title text1="Admin" text2="Dashboard" />
+
+      <div className="relative flex flex-wrap gap-4 mt-6">
+        <BlurCircle top="-100px" left="0px"/>
+        <div className="flex flex-wrap gap-4 w-full">
+            {dashboardCards.map((card, index) => (
+                <div key={index} className="flex items-center justify-between px-4 py-3 bg-primary/10 border border-primary/20 rounded-md max-w-50 w-full">
+                    <div>
+                        <h1 className="text-sm">{card.title}</h1>
+                        <p className="text-xl font-medium mt-1">{card.value}</p>
+                    </div>
+                    <card.icon className="w-6 h-6"/>
+                </div>
+            ))}
+        </div>
+      </div>
     </>
   ) : (
     <Loading />
