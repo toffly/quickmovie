6befc5c560ec7b1a644ed88ed06350d0a10fdff6
toffly <toffly@outlook.com>
2026-08-11 14:40:42 +0700

    feat: implement Dashboard component with loading state and dashboard cards; add Title component for header

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,7 +1,67 @@
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+import {
+  ChartLineIcon,
+  CircleDollarSignIcon,
+  PlayCircleIcon,
+  UsersIcon,
+} from "lucide-react";
+import { useEffect, useState } from "react";
+import { dummyDashboardData } from "../../assets/assets";
+import Loading from "../../components/Loading";
+import Title from "../../components/admin/Title";
 
-export default Dashboard
+const Dashboard = () => {
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const [dashboardData, setDashboardData] = useState({
+    totalBookings: 0,
+    totalRevenue: 0,
+    activeShows: [],
+    totalUser: 0,
+  });
+  const [loading, setLoading] = useState(true);
+
+  const dashboardCards = [
+    {
+      title: "Total Bookings",
+      value: dashboardData.totalBookings || "0",
+      icon: ChartLineIcon,
+    },
+    {
+      title: "Total Revenue",
+      value: dashboardData.totalRevenue || "0",
+      icon: CircleDollarSignIcon,
+    },
+    {
+      title: "Active Shows",
+      value: dashboardData.activeShows.length || "0",
+      icon: PlayCircleIcon,
+    },
+    {
+      title: "Total Users",
+      value: dashboardData.totalUser || "0",
+      icon: UsersIcon,
+    },
+  ];
+
+  const fetchDashboardData = async () => {
+    setDashboardData(dummyDashboardData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    const fetcData = async () => {
+      await fetchDashboardData();
+    };
+    fetcData();
+  }, []);
+
+  return !loading ? (
+    <>
+      <Title text1="Admin" text2="Dashboard" />
+    </>
+  ) : (
+    <Loading />
+  );
+};
+
+export default Dashboard;
