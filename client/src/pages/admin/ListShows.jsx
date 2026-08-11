b39c40b432d5a9e49a4db52af42509624d6cb825
toffly <toffly@outlook.com>
2026-08-11 15:02:12 +0700

    feat: implement ListShows component with loading state and show data retrieval

--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -1,7 +1,48 @@
-const ListShows = () => {
-  return (
-    <div>ListShows</div>
-  )
-}
+import { useEffect, useState } from "react";
+import { dummyShowsData } from "../../assets/assets";
+import Loading from "../../components/Loading";
+import Title from "../../components/admin/Title";
 
-export default ListShows
+const ListShows = () => {
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const [shows, setShows] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getAllShows = async () => {
+    try {
+      setShows([
+        {
+          movie: dummyShowsData[0],
+          showDateTime: "2025-06-30T02:30:00.000Z",
+          showPrice: 59,
+          occupiedSeats: {
+            A1: "user_1",
+            B1: "user_2",
+            C1: "user_3",
+          },
+        },
+      ]);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      await getAllShows();
+    };
+    getData();
+  }, []);
+
+  return !loading ? (
+    <>
+      <Title text1="List" text2="Shows" />
+    </>
+  ) : (
+    <Loading />
+  );
+};
+
+export default ListShows;
