4409bcf1f1d74142310f71d9e56b47bad77854c1
toffly <toffly@outlook.com>
2026-08-12 13:26:40 +0700

    feat: implement date and time selection for shows in AddShows component

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -17,6 +17,34 @@ const AddShows = () => {
     setNowPlayingMovies(dummyShowsData);
   };
 
+  const handleDateTimeAdd = () => {
+    if (!dateTimeInput) return;
+    const [date, time] = dateTimeInput.split("T");
+    if (!date || !time) return;
+
+    setDateTimeSelection((prev) => {
+      const times = prev[date] || [];
+      if (!times.includes(time)) {
+        return { ...prev, [date]: [...times, time] };
+      }
+      return prev;
+    });
+  };
+
+  const handleRemoveTime = (date,time) => {
+    setDateTimeSelection((prev) => {
+      const filteredTimes = prev[date].filter((t) => t !== time)
+      if(filteredTimes.length === 0) {
+        const { [date]: _, ...rest} = prev
+        return rest
+      }
+      return {
+        ...prev,
+        [date]: filteredTimes
+      }
+    })
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       await fetchNowPlayingMovies();
@@ -25,7 +53,7 @@ const AddShows = () => {
   }, []);
 
   return nowPlayingMovies.length > 0 ? (
-    <div>
+    <>
       <Title text1="Add" text2="Shows"></Title>
       <p className="mt-10 text-lg font-medium">Now Playing Movies</p>
       <div className="overflow-x-auto pb-4">
@@ -54,7 +82,7 @@ const AddShows = () => {
               </div>
               {selectedMovie === movie.id && (
                 <div className="absolute top-2 right-2 flex  items-center justify-center bg-primary h-6 w-6 rounded">
-                  <CheckIcon className="w-4 h-4 rounded" strokeWidth={2.5}/>
+                  <CheckIcon className="w-4 h-4 rounded" strokeWidth={2.5} />
                 </div>
               )}
               <p className="font-medium truncate">{movie.title}</p>
@@ -63,7 +91,41 @@ const AddShows = () => {
           ))}
         </div>
       </div>
-    </div>
+      <div className="mt-8">
+        <label className="block text-sm font-medium mb-2">Show Price</label>
+        <div className="inline-flex items-center gap-2 border border-gray-600 px-3 py-2 rounded-md">
+          <p className="text-gray-400 text-sm">{currency}</p>
+          <input
+            min={0}
+            type="number"
+            value={showPrice}
+            onChange={(e) => setShowPrice(e.target.value)}
+            placeholder="Enter show price"
+            className="outline-none"
+          />
+        </div>
+      </div>
+
+      <div className="mt-6">
+        <label className="block text-sm font-medium mb-2">
+          Select Date and Time
+        </label>
+        <div className="inline-flex gap-5 border border-gray-600 p-1 pl-3 rounded-lg">
+          <input
+            type="datetime-local"
+            value={dateTimeInput}
+            onChange={(e) => setDateTimeInput(e.target.value)}
+            className="outline-none rounded-md"
+          />
+          <button
+            onClick={handleDateTimeAdd}
+            className="bg-primary/80 text-white px-3 py-2 text-sm rounded-lg hover:bg-primary cursor-pointer"
+          >
+            Add Time
+          </button>
+        </div>
+      </div>
+    </>
   ) : (
     <Loading />
   );
