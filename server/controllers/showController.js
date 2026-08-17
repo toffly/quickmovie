4898162cdbd1e56b74ac7399adb50f3ba99cd36c
toffly <toffly@outlook.com>
2026-08-17 12:25:18 +0700

    refactor: improve axios request structure in getNowPlayingMovies function

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,15 +1,23 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const getNowPlayingMovies = async (req, res) => {
-    try {
-        const { data } =  await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
-            header: {Authorization: `Bearer ${process.env.TMDB_API_KEY}`}
-        })
+  const url = "https://api.themoviedb.org/3/movie/now_playing";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+  };
 
-        const movies = data.results
-        res.json({success: true, movies: movies})
-    } catch (error) {
-        console.log(error)
-        res.json({success: false, message:error.message})
-    }
-}
+  try {
+    const { data } = await axios.get(url, options);
+
+    const movies = data.results;
+    res.json({ success: true, movies: movies });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
