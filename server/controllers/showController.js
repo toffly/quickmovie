7698efc8f0dcf0394703d88de306b3212516b99d
toffly <toffly@outlook.com>
2026-08-18 15:40:34 +0700

    feat: add getShows and getShow functions to retrieve show details and unique movies

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -92,3 +92,44 @@ export const addShow = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+export const getShows = async (req, res) => {
+  try {
+    const shows = (
+      await Show.find({ showDateTime: { $gte: new Date() } }).populate("movie")
+    ).toSorted({ showDateTime: 1 });
+
+    const uniqueShows = new Set(shows.map((show) => show.movie));
+
+    res.json({
+      success: true,
+      shows: Array.from(uniqueShows),
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getShow = async(req, res) => {
+  try {
+    const {movieId} = req.params
+
+    const shows = await Show.find({movie: movieId, showDateTime: {$gte: new Date ()}})
+
+    const movie = await Movie.findById(movieId)
+    const dateTime = {}
+
+    shows.forEach((show) => {
+      const data = show.showDateTime.toISOString().split("T")[0]
+      if(!dateTime[date]) {
+        dateTime[date] = []
+      }
+      dateTime[date].push({ time: show.showDateTime, showId: show._id})
+    })
+    res.json({success: true, movie, dateTime})
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+}
