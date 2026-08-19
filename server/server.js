67767698696ba8748ea7e7599e065bfcd2941b63
toffly <toffly@outlook.com>
2026-08-19 14:17:15 +0700

    feat: add adminRouter for handling admin-related API routes

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import showRouter from "./routes/showRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
 
 const app = express();
 const port = 3000;
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/show", showRouter);
 app.use("/api/booking", bookingRouter);
+app.use("/api/admin", adminRouter);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
