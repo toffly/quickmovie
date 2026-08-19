83fb21899292729221ddf265acc9f8a40eec0462
toffly <toffly@outlook.com>
2026-08-19 14:12:01 +0700

    feat: add getAllShows function to retrieve upcoming shows

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -28,3 +28,15 @@ export const getDashboardData = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+export const getAllShows = async (req, res) => {
+  try {
+    const shows = (
+      await Show.find({ showDateTime: { $gte: new Date() } }).populate("movie")
+    ).sort({ showDateTime: 1 });
+    res.json({ success: true, shows });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
