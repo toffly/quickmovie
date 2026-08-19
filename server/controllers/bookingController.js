c2b5ba8c149a2fac43c4853ab339374ee966f369
toffly <toffly@outlook.com>
2026-08-19 13:56:14 +0700

    feat: implement createBooking function to handle booking creation and seat availability checks

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,3 +1,4 @@
+import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
 
 const checkSeatsAvailability = async (showId, selectedSeats) => {
@@ -14,3 +15,36 @@ const checkSeatsAvailability = async (showId, selectedSeats) => {
     return false;
   }
 };
+
+export const createBooking = async (req, res) => {
+    try {
+        const {userId} = req.auth()
+        const {showId, selectedSeats} = req.body
+        const {origin} = req.headers
+
+        const isAvailable = await checkSeatsAvailability(showId, selectedSeats)
+
+        if(!isAvailable) {
+            return res.json({success: false, message: "Selected Seats are not available."})
+        }
+
+        const showData = await Show.findById(showId).populate('movie')
+
+        const booking = await Booking.create({
+            user: userId,
+            show: showId,
+            amount: showData.showPrice * selectedSeats.length,
+            bookedSeats: selectedSeats,
+        })
+
+        selectedSeats.map((seat) => {
+            showData.occupiedSeats[seat] = userId
+        })
+
+        showData,markModified('occupiedSeats')
+
+        await showData.save()
+    } catch (error) {
+        
+    }
+}
