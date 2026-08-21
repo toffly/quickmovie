e4d89577bba3b309c04768cf7644a685744dcfd1
toffly <toffly@outlook.com>
2026-08-21 16:19:33 +0700

    feat: add AppContext and AppProvider for global state management

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,98 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { useAuth, useUser } from "@clerk/react";
+import { useLocation, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+axios.defaults.baseUrl = import.meta.env.VITE_BASE_URL;
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [shows, setShows] = useState([]);
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+
+  const { user } = useUser();
+  const { getToken } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const fetchIsAdmin = async () => {
+    try {
+      const { data } = await axios.get("/api/admin/is-admin", {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      });
+      setIsAdmin(data.isAdmin);
+
+      if (!data.isAdmin && location.pathname.startsWith("/admin")) {
+        navigate("/");
+        toast.error("You are not authorized to access admin dashboard.");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchShow = async () => {
+    try {
+      const { data } = await axios.get("/api/show/all");
+      if (data.success) {
+        setShows(data.shows);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchFavoriteMovies = async () => {
+    try {
+      const { data } = await axios.get("/api/user/favorites", {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      });
+
+      if (data.success) {
+        setFavoriteMovies(data.movies);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const dataShow = async () => {
+      await fetchShow();
+    };
+    dataShow();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      const dataAdmin = async () => {
+        await fetchIsAdmin();
+        await fetchFavoriteMovies();
+      };
+      dataAdmin();
+    }
+  }, [user]);
+
+  const value = {
+    axios,
+    fetchIsAdmin,
+    user,
+    getToken,
+    navigate,
+    isAdmin,
+    shows,
+    favoriteMovies,
+    fetchFavoriteMovies,
+  };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => useContext(AppContext);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,15 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/react";
+import { AppProvider } from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ClerkProvider>
-      <App />
-    </ClerkProvider>
-  </BrowserRouter>,
+  <ClerkProvider>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
+    ,
+  </ClerkProvider>,
 );
